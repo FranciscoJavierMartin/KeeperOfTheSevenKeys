96fb1e15bee6ed27dd95e47f9edcfeaa56fed056
Francisco <javiermartincuenca@gmail.com>
2018-10-21 14:00:54 +0200

    Solved

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Basic information
+## Author
+* Name: Francisco Javier Martin Cuenca.
+* Email: franciscomartincuenca@protonmail.com
+
+## Solution description
+First of all, check if keeper has all keys, then go to the door, else go for the keys. The keeper looks on each direction to check if there are the door or keys depends of the situation. If any interesing object is around choose a random direction to walk that not contains a wall. 
--- src/main/java/tws/keeper/solution/KeeperAI.java
+++ src/main/java/tws/keeper/solution/KeeperAI.java
@@ -12,7 +12,6 @@ public class KeeperAI implements Keeper {
 
     private static final List<Action> availableActions = Arrays.asList(GO_DOWN, GO_LEFT, GO_RIGHT, GO_UP);
 
-
     /**
      * This Keeper Artificial Inteligence simply acts randomly
      *
@@ -20,7 +19,53 @@ public class KeeperAI implements Keeper {
      * @return
      */
     public Action act(Observable maze) {
-        return availableActions.get(ThreadLocalRandom.current().nextInt(availableActions.size()));
+        Action nextAction = DO_NOTHING;
+
+        if (!maze.isMazeCompleted()) {
+            if (maze.getKeysFound() == maze.getTotalNumberOfKeys()) {
+                if (maze.lookDown().equals(Cell.DOOR)) {
+                    nextAction = GO_DOWN;
+                } else if (maze.lookUp().equals(Cell.DOOR)) {
+                    nextAction = GO_UP;
+                } else if (maze.lookRight().equals(Cell.DOOR)) {
+                    nextAction = GO_RIGHT;
+                } else if (maze.lookLeft().equals(Cell.DOOR)) {
+                    nextAction = GO_LEFT;
+                }
+            } else {
+                if (maze.lookDown().equals(Cell.KEY)) {
+                    nextAction = GO_DOWN;
+                } else if (maze.lookUp().equals(Cell.KEY)) {
+                    nextAction = GO_UP;
+                } else if (maze.lookRight().equals(Cell.KEY)) {
+                    nextAction = GO_RIGHT;
+                } else if (maze.lookLeft().equals(Cell.KEY)) {
+                    nextAction = GO_LEFT;
+                } else {
+                    List<Action> possibleDirections = new ArrayList<>();
+                    if (!maze.lookDown().equals(Cell.WALL)) {
+                        possibleDirections.add(GO_DOWN);
+                    }
+
+                    if (!maze.lookUp().equals(Cell.WALL)) {
+                        possibleDirections.add(GO_UP);
+                    }
+
+                    if (!maze.lookRight().equals(Cell.WALL)) {
+                        possibleDirections.add(GO_RIGHT);
+                    }
+
+                    if (!maze.lookLeft().equals(Cell.WALL)) {
+                        possibleDirections.add(GO_LEFT);
+                    }
+
+                    nextAction = possibleDirections
+                            .get(ThreadLocalRandom.current()
+                                    .nextInt(possibleDirections.size()));
+                }
+            }
+        }
+        return nextAction;
     }
 
 }
